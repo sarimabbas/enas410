908e0e0876c6e6e574d3c32dfa08a20b36248a91
Sarim Abbas <sarim.stays@gmail.com>
2018-11-21 21:27:00 -0500

    preliminary scroll fade

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 
 // styles
@@ -6,22 +6,62 @@ import styles from './Nav.module.css'
 import classNames from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const nav = (props) => { 
-    return (
-        <Aux>
-            <div className={classNames(styles.nav)}>
-                <div className={classNames(styles.links)}>
-                    <span className={classNames(styles.navLink)}>Visit</span>
-                    <span className={classNames(styles.navLink)}>Exhibitions</span>
-                    <span className={classNames(styles.navLink)}>Learn</span>
-                    <span className={classNames(styles.navLink)}>Collections</span>
-                    <span className={classNames(styles.navLink)}>About</span>
-                    <FontAwesomeIcon icon={["fas", "bars"]} className={classNames(styles.navLink)}></FontAwesomeIcon>
-                </div>
-            </div>
 
-        </Aux>
-    )
+class nav extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            scrollThresholdMet : false,
+        }
+
+        this.handleScroll = this.handleScroll.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }   
+
+    handleScroll = (event) => {
+        let scrollTop = document.scrollingElement.scrollTop
+
+        if (!this.state.scrollThresholdMet && scrollTop > 500) {
+            this.setState({
+                scrollThresholdMet : true,
+            })
+        } 
+
+        if (this.state.scrollThresholdMet && scrollTop <= 500) {
+            this.setState({
+                scrollThresholdMet: false,
+            })
+        }
+
+        // console.log(scrollTop);
+    }
+
+    render() {
+        
+        let opaque = this.state.scrollThresholdMet ? styles.opaque : NaN;
+        return (
+            <Aux>
+                <div className={classNames(styles.nav, opaque)}>
+                    <div className={classNames(styles.links)}>
+                        <span className={classNames(styles.link)}>Visit</span>
+                        <span className={classNames(styles.link)}>Exhibitions</span>
+                        <span className={classNames(styles.link)}>Learn</span>
+                        <span className={classNames(styles.link)}>Collections</span>
+                        <span className={classNames(styles.link)}>About</span>
+                        <FontAwesomeIcon icon={["fas", "bars"]} className={classNames(styles.link)}></FontAwesomeIcon>
+                    </div>
+                </div>
+
+            </Aux>
+        )
+    }
 }
 
 export default nav;
