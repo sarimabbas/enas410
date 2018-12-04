c2df3b79c458e0deb34ce402d0c3817aa62ec1d3
Sarim Abbas <sarim.stays@gmail.com>
2018-12-04 12:14:27 -0500

    structure

---
+++ src/pages/Exhibits/Exhibits.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Route } from 'react-router-dom';
+import classNames from 'classnames';
+
+// components
+import Aux from '../../hoc/Aux';
+import Generic from '../Generic/Generic';
+import Sidebar from './Sidebar';
+
+// import sub pages here
+
+// styles
+import styles from './Exhibits.module.css'
+
+const exhibits = (props) => {
+    return (    
+        <Aux>
+            <Generic>
+                <div className={classNames(styles.grid)}>
+                    <Sidebar></Sidebar>
+                    <div className={classNames(styles.content)}>
+                    {/* start sub pages components here */}
+
+                    { /* end sub pages components here */ }
+                    </div>
+                </div>
+            </Generic>
+        </Aux>
+    )
+}
+
+export default exhibits;
