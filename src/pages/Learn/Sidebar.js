5767d8dfe21855e6e66d51af114e34809912d66f
Kevin Koste <kevin.koste@yale.edu>
2018-12-06 12:38:35 -0500

    sidebar tweaks

--- src/pages/Learn/Sidebar.js
+++ src/pages/Learn/Sidebar.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+
 import classNames from 'classnames';
-import { Link } from 'react-router-dom';
 import styles from '../Sidebar.module.css';
+
+// routing
+import { Link } from 'react-router-dom';
 
 const sidebar = (props) => {
 
@@ -13,8 +16,8 @@ const sidebar = (props) => {
                     Learn
                 </h1>
                 <div className={classNames(styles.links)}>
-                    <Link to="/learn/adult"><span>Adult</span></Link>
                     <Link to="/learn/group"><span>Group</span></Link>
+                    <Link to="/learn/adult"><span>Adult</span></Link>
                     <Link to="/learn/yale"><span>Yale</span></Link>
                 </div>
             </div>
--- src/pages/Visit/Sidebar.js
+++ src/pages/Visit/Sidebar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 
 import classNames from 'classnames';
-import styles from './Sidebar.module.css';
+import styles from '../Sidebar.module.css';
 
 // routing
 import { Link } from 'react-router-dom';
@@ -16,8 +16,8 @@ const sidebar = (props) => {
                     Visit
                 </h1>
                 <div className={classNames(styles.links)}>
-                    <Link to="/map"><span>Map</span></Link>
                     <Link to="/visit/admission"><span>Admission {'&'} Hours</span></Link>
+                    <Link to="/map"><span>Map</span></Link>
                     <Link to="/visit/directions"><span>Directions {'&'} Parking</span></Link>
                 </div>
             </div>
