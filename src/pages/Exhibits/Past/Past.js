91bcd54e3a05e5085c751b2fe3c765d704301c47
Sarim Abbas <sarim.stays@gmail.com>
2018-12-08 14:03:00 -0500

    changes

---
+++ src/pages/Exhibits/Past/Past.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Aux from '../../../hoc/Aux';
+
+
+import classNames from 'classnames';
+import styles from './Past.module.css';
+
+// routing
+import { Link } from 'react-router-dom';
+
+const past = (props) => {
+    return (
+        <Aux>
+            <h1 className={classNames(styles.title)}>Past</h1>
+        </Aux>
+    )
+}
+
+export default past;
