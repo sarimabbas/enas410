a84c767e4903b0be94cdbed7ae23101710dcc9a2
Sarim Abbas <sarim.stays@gmail.com>
2018-12-10 15:01:48 -0500

    i had tea for breakfast

--- src/pages/FloorMap/FloorMap.js
+++ src/pages/FloorMap/FloorMap.js
@@ -4,11 +4,15 @@ import { Link } from 'react-router-dom';
 import { slide as Menu } from 'react-burger-menu'
 import classNames from 'classnames';
 import Aux from '../../hoc/Aux';
+import Generic from '../Generic/Generic';
+import { Route, Redirect, Switch } from 'react-router-dom';
 
 // all the components that make up the page
 import NavSimple from '../../components/Nav/NavSimple';
 // import Footer from '../../components/Footer/Footer';
 import FirstFloor from './FirstFloor/FirstFloor';
+import SecondFloor from './SecondFloor/SecondFloor';
+import Sidebar from './Sidebar';
 
 // styling
 import styles from './FloorMap.module.css';
@@ -58,6 +62,8 @@ class floormap extends Component {
  
     render() {
 
+        const firstFloor = <FirstFloor handleRoom={this.handleRoom}/>;
+
         let more = <div></div>
         if (this.state.more !== "") {
             console.log('wihf')
@@ -67,22 +73,38 @@ class floormap extends Component {
 
         return (
             <Aux>
-                <NavSimple />
-                <Menu   isOpen={this.state.roomSelected} 
+                <Generic>
+                    {/* Menu overlay */}
+                    <Menu   isOpen={this.state.roomSelected} 
                         noOverlay
                         right
                         onStateChange={ this.handleMenuChange }
                         >
-                    <img src={this.state.image} className={classNames(styles.image)} alt="overlay"></img>
-                    <div className={classNames(styles.content)}>
-                        <h1 className={classNames(styles.title)}>{this.state.title}</h1> 
-                        <p className={classNames(styles.description)}>{this.state.description}</p>
-                        <div className={classNames(styles.more)}>{ more }</div>
+                        <img src={this.state.image} className={classNames(styles.image)} alt="overlay"></img>
+                        <div className={classNames(styles.menuContent)}>
+                            <h1 className={classNames(styles.title)}>{this.state.title}</h1> 
+                            <p className={classNames(styles.description)}>{this.state.description}</p>
+                            <div className={classNames(styles.more)}>{ more }</div>
+                        </div>
+                    </Menu>
+                    {/* grid of sidebar and map */}
+                    <div className={classNames(styles.grid)}>
+                        <Sidebar></Sidebar>
+                        <div className={classNames(styles.gridRight)}>
+                            <div className={styles.floor}>
+                            <Switch>
+                                <Redirect exact from="/map" to="/map/first-floor"/>
+                                <Route path='/map/first-floor' 
+                                    render={(props) => <FirstFloor {...props} handleRoom={this.handleRoom} />}/>
+                                <Route path='/map/second-floor' 
+                                    render={(props) => <SecondFloor {...props} handleRoom={this.handleRoom} />}/>
+                            </Switch>
+                            </div>
+                        </div>
+                        {/* empty div */}
+                        <div></div>
                     </div>
-                </Menu>
-                <div className={classNames(styles.wrapper)}>
-                    <FirstFloor handleRoom={this.handleRoom}/>
-                </div>
+                </Generic>
             </Aux>
         )
     }
---
+++ src/pages/FloorMap/Sidebar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
+import classNames from 'classnames';
+import styles from '../Sidebar.module.css';
+
+// routing
+import { NavLink } from 'react-router-dom';
+
+const sidebar = (props) => {
+
+    return (
+        <Aux>
+            <div className={classNames(styles.sidebar)}>
+                <h3 className={classNames(styles.heading)}>
+                    Floors
+                </h3>
+                <div className={classNames(styles.links)}>
+                    <NavLink activeClassName={styles.activeLink} to="/map/first-floor"><span>First Floor</span></NavLink>
+                    <NavLink activeClassName={styles.activeLink} to="/map/second-floor"><span>Second Floor</span></NavLink>
+                    <NavLink activeClassName={styles.activeLink} to="/map/third-floor"><span>Third Floor</span></NavLink>
+                </div>
+            </div>
+        </Aux>
+    )
+}
+
+export default sidebar;
