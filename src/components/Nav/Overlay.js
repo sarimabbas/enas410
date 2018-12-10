b41b0c9695c7cad186ce5a1d6b55e6477c786c56
Sarim Abbas <sarim.stays@gmail.com>
2018-12-09 22:35:39 -0500

    overlay fixes and scroll

--- src/components/Nav/Overlay.js
+++ src/components/Nav/Overlay.js
@@ -15,6 +15,7 @@ class overlay extends Component {
         return (
             <Aux>
                 <div className={classNames(styles.overlay)}>
+                <div className={classNames(styles.overlayContent)}>
 
                     {/* controls */}
                     <div className={classNames(styles.controls)}>
@@ -41,68 +42,68 @@ class overlay extends Component {
                         {/* links */}
                         <div className={classNames(styles.right)}>
 
-                        <div className={classNames(styles.grid)}>
+                            <div className={classNames(styles.grid)}>
 
-                                    <div>
-                                        <span className={classNames(styles.heading)}>
-                                        <Link to="/visit">Visit</Link>
-                                        </span>
-                                        <div className={classNames(styles.links)}>
-                                            <Link to="/visit/admission">Admission {'&'} Hours</Link>
-                                            <Link to="/map">Floor Plan {'&'} Virtual Tour</Link>
-                                            <Link to="/visit/cafe">Peabody Café</Link>
-                                            <Link to="/visit/directions">Directions {'&'} Parking</Link>
-                                        </div>
+                                <div>
+                                    <span className={classNames(styles.heading)}>
+                                    <Link to="/visit">Visit</Link>
+                                    </span>
+                                    <div className={classNames(styles.links)}>
+                                        <Link to="/visit/admission">Admission {'&'} Hours</Link>
+                                        <Link to="/map">Floor Plan {'&'} Virtual Tour</Link>
+                                        <Link to="/visit/cafe">Peabody Café</Link>
+                                        <Link to="/visit/directions">Directions {'&'} Parking</Link>
                                     </div>
-
-                                    <div>
-                                        <span className={classNames(styles.heading)}>
-                                            Exhibits
-                                        </span>
-                                        <div className={classNames(styles.links)}>
-                                            <Link to="/exhibits/current">Exhibits On View</Link>
-                                            <Link to="/exhibits/temporary">Temporary Exhibits</Link>
-                                            <Link to="/exhibits/upcoming">Past Exhibits</Link>
-                                            <Link to="/exhibits/calendar">Events Calendar</Link>
-                                        </div>
-                                    </div>
-
-                                    <div>
-                                        <span className={classNames(styles.heading)}>
-                                            Learn
-                                        </span>
-                                        <div className={classNames(styles.links)}>
-                                            <Link to="/learn/k12">K-12 Programs</Link>
-                                            <Link to="https://evolutions.peabody.yale.edu/">Peabody Evolutions</Link>
-                                            <Link to="/learn/yale">Yale Student Opportunities</Link>
-                                            <Link to="/learn/adult">Adult Programs</Link>
-                                        </div>
-                                    </div>
-
-                                    <div>
-                                        <span className={classNames(styles.heading)}>
-                                            Collections
-                                        </span>
-                                        <div className={classNames(styles.links)}>
-                                            <Link to="/collections/search">Search the Collections</Link>
-                                            <Link to="/collections/how-to-use">How to Use</Link>
-                                            <Link to="/collections/daily-object">Object of the Day</Link>
-                                        </div>
-                                    </div>
-
-                                    <div>
-                                        <span className={classNames(styles.heading)}>
-                                            About
-                                        </span>
-                                        <div className={classNames(styles.links)}>
-                                            <Link to="/about/highlights">Peabody Highlights</Link>
-                                            <Link to="/about/mission">Mission {'&'} History</Link>
-                                            <Link to="/about/contact">Contact Us</Link>
-                                            <Link to="/about/staff">Staff</Link>
-                                        </div>
-                                    </div>
-
                                 </div>
+
+                                <div>
+                                    <span className={classNames(styles.heading)}>
+                                        <Link to="/exhibits">Exhibits</Link>
+                                    </span>
+                                    <div className={classNames(styles.links)}>
+                                        <Link to="/exhibits/current">Exhibits On View</Link>
+                                        <Link to="/exhibits/temporary">Temporary Exhibits</Link>
+                                        <Link to="/exhibits/upcoming">Past Exhibits</Link>
+                                        <Link to="/exhibits/calendar">Events Calendar</Link>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <span className={classNames(styles.heading)}>
+                                        Learn
+                                    </span>
+                                    <div className={classNames(styles.links)}>
+                                        <Link to="/learn/k12">K-12 Programs</Link>
+                                        <Link to="https://evolutions.peabody.yale.edu/">Peabody Evolutions</Link>
+                                        <Link to="/learn/yale">Yale Student Opportunities</Link>
+                                        <Link to="/learn/adult">Adult Programs</Link>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <span className={classNames(styles.heading)}>
+                                        Collections
+                                    </span>
+                                    <div className={classNames(styles.links)}>
+                                        <Link to="/collections/search">Search the Collections</Link>
+                                        <Link to="/collections/how-to-use">How to Use</Link>
+                                        <Link to="/collections/daily-object">Object of the Day</Link>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <span className={classNames(styles.heading)}>
+                                        About
+                                    </span>
+                                    <div className={classNames(styles.links)}>
+                                        <Link to="/about/highlights">Peabody Highlights</Link>
+                                        <Link to="/about/mission">Mission {'&'} History</Link>
+                                        <Link to="/about/contact">Contact Us</Link>
+                                        <Link to="/about/staff">Staff</Link>
+                                    </div>
+                                </div>
+
+                            </div>
 
                         </div>
 
@@ -112,7 +113,7 @@ class overlay extends Component {
 
 
                    
-
+                </div>
                 </div>
             </Aux>
         )
