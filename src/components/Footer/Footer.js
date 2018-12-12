dadc19d265829838bfd32d68a2bee1e4a4688eb2
rebeccareza <rebecca.reza@yale.edu>
2018-12-11 20:59:27 -0500

    footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -53,10 +53,10 @@ const footer = (props) => {
                     {/* social & copyright */}
                     <div className={styles.social}>
                         <div className={styles.icons}>
-                            <FontAwesomeIcon icon={["fab", "facebook"]} size="2x"></FontAwesomeIcon>
-                            <FontAwesomeIcon icon={["fab", "instagram"]} size="2x"></FontAwesomeIcon>
-                            <FontAwesomeIcon icon={["fab", "twitter"]} size="2x"></FontAwesomeIcon>
-                            <FontAwesomeIcon icon={["fab", "youtube"]} size="2x"></FontAwesomeIcon>
+                            <a href="https://www.facebook.com/YalePeabodyMuseum/" style={{textDecoration: "none"}}><FontAwesomeIcon icon={["fab", "facebook"]} size="2x"></FontAwesomeIcon></a>
+                            <a href ="https://www.instagram.com/enas410peabody/" style={{textDecoration: "none"}}><FontAwesomeIcon icon={["fab", "instagram"]} size="2x"></FontAwesomeIcon></a>
+                            <a href="https://twitter.com/yalepeabody?lang=en" style={{textDecoration: "none"}}><FontAwesomeIcon icon={["fab", "twitter"]} size="2x" ></FontAwesomeIcon></a>
+                            <a href ="https://www.youtube.com/channel/UC5c5hS-NG0ya1cyvn85koMA" style={{textDecoration: "none"}}><FontAwesomeIcon icon={["fab", "youtube"]} size="2x"></FontAwesomeIcon></a>                          
                         </div>
                         <br/><br/>
                         <span>Copyright &copy; 2018<br/>Yale Peabody Museum <br/> of Natural History</span>
