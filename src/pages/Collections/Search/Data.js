733b689bccf921814c41c23e61f7eae938c1df05
rebeccareza <rebecca.reza@yale.edu>
2018-12-11 20:11:57 -0500

    collections shows something now!

---
+++ src/pages/Collections/Search/Data.js
@@ -0,0 +1,121 @@
+// images
+import acanthite from '../Assets/acanthite.jpg'
+import smithsonite from '../Assets/smithsonite.jpg'
+import zoomorphic from '../Assets/zoomorphic-bowl.jpg'
+import bag from '../Assets/bag.jpg'
+import tablet from '../Assets/tablet.jpg'
+import cylinder from '../Assets/cylinderseal.jpg'
+import abies from '../Assets/abies.jpg'
+import algae from '../Assets/algae.jpg'
+import cinnamomum from '../Assets/cinnamomum.jpg'
+import Liriodendron from '../Assets/Liriodendron.jpg'
+import Abisara from '../Assets/Abisara.jpg'
+
+
+const data = [
+
+        {
+            id: "/2019/01/01/acanthite",
+            title: "Acanthite", 
+            subtitle: "Mineralogy and Meteoritics",
+            image: acanthite,
+            short: "YPM MIN 040571",
+            long: "North America; Canada; British Columbia; Okanagan-Similkameen District; Beaverdell; Highland Bell Mine",
+        } ,
+
+        {
+            id: "/2019/01/01/smithsonite",
+            title: "Smithsonite", 
+            subtitle: "Mineralogy and Meteoritics",
+            image: smithsonite,
+            short: "YPM MIN 030888",
+            long: "Australia; New South Wales; Broken Hill",
+        } ,
+
+        {
+            id: "/2019/01/01/zoomorphic-bowl",
+            title: "Zoomorphic bowl with handles in the form of animal heads, clay, 5' high, 3 1/4' diameter, Late Archaic culture of Colima, Western Mexico, ca. 300-600 A.D.", 
+            subtitle: "Anthropology",
+            image: zoomorphic,
+            short: "YPM ANT 207013",
+            long: "North America; Mexico; Colima",
+        } ,
+
+        {
+        	id: "/2004/11/15/Butbuttung",
+            title: "<i>Butbuttung</i> - small man's hip-bag. White with black and red and yellow embroidery. White fringe. Brass carrying ring. L: 55 cm W: 23 cm.", 
+            subtitle: "Anthropology",
+            image: bag,
+            short: "YPM ANT 261125",
+            long: "Asia, Philippines",
+        } ,
+
+ 
+
+        {
+        	id: "/2014/10/31/tablet",
+            title: "2 column tablet. Account of barley and wheat. Ur III. Clay.", 
+            subtitle: "Babylonian Collection",
+            image: tablet,
+            short: "YPM BC 015930",
+            long: "Asia; Iraq; Umma (Tell Jokha)",
+        } ,
+
+
+        {
+        	id: "/2018/10/10/cylinderseal",
+            title: "Cylinder seal. Goddess with two raised hand, long flounced dress and horned headdress, god in ascending pose with long skirt parted over knee, and holding weapon facing figure almost lost: inscription. Old Babylonian. Hematite.", 
+            subtitle: "Babylonian Collection",
+            image: cylinder,
+            short: "YPM BC 029880",
+            long: "",
+        } ,
+
+       {
+        	id: "/2018/11/16/abies",
+            title: "Abies concolor (Gord. & Glend.) Lindl. ex Hildebr.", 
+            subtitle: "Botany",
+            image: abies,
+            short: "YU.104586",
+            long: "North America; USA; California; slopes of Red Lassic, 5560-5580 ft",
+        } ,
+
+        {
+        	id: "/2018/06/12/algae",
+            title: "Algae", 
+            subtitle: "Botany",
+            image: algae,
+            short: "YU.053645",
+            long: "",
+        } ,
+
+        {
+        	id: "/2018/11/28/Cinnamomum",
+            title: "Cinnamomum hesperium Van Boskirk", 
+            subtitle: "Paleobotany",
+            image: cinnamomum,
+            short: "YPM PB 045048.A",
+            long: "North America; USA; Wyoming; Park County; Elk Basin; Elk Basin",
+        } ,
+
+        {
+        	id: "/2018/11/26/liriodendron",
+            title: "Liriodendron laramiense Ward", 
+            subtitle: "Paleobotany",
+            image: Liriodendron,
+            short: "YPM PB 006361",
+            long: "	North America; USA; North Dakota; Bowman County; 1400mS 1525mE NW corner of sec, 15m below K/T datum",
+        } ,
+
+        {
+        	id: "/2017/12/20/Abisara",
+            title: "Abisara echerius (Stoll)", 
+            subtitle: "Entomology",
+            image: Abisara,
+            short: "YPM ENT 756334",
+            long: "Asia; Hong Kong; Cheung Chau Islands",
+        } ,
+
+]
+
+export default data;
--- src/pages/Collections/Search/Search.js
+++ src/pages/Collections/Search/Search.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {Component} from 'react'
 import Aux from '../../../hoc/Aux';
+import SpotlightCard from '../../../components/Spotlight/SpotlightCard';
 
 
 import classNames from 'classnames';
@@ -8,16 +9,91 @@ import styles from './search.module.css';
 // routing
 import { Link } from 'react-router-dom';
 
-// assets
+// data
+import Data from './Data';
 
+class search extends Component {
 
-const search = (props) => {
-    return (
-        <Aux>
-            <h1>Search the Collections</h1>
-            {/* <img src={sample}></img> */}
-        </Aux>
-    )
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            gridView : true,
+            title : "",
+            subtitle : "",
+            image : "",
+            description : "",
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+        this.handleBack2Grid = this.handleBack2Grid.bind(this);
+    }
+    
+    handleClick = (event) => {
+        let id = event.currentTarget.id
+        let post  = NaN
+        for(let i = 0; i < Data.length; i++) {
+            if (Data[i].id === id) {
+                post = Data[i];
+            }
+        }
+
+        this.setState({
+            gridView : false,
+            title : post.title,
+            subtitle : post.subtitle,
+            image : post.image,
+            description : post.long,
+        })
+    }
+
+    handleBack2Grid = (event) => {
+        this.setState({
+            gridView : true,
+        })
+    }
+
+    pullPosts = () => {
+
+        console.log(Data.length)
+        let posts_array = []
+        for (let i = 0; i < Data.length; i++) {
+            posts_array.push(
+                <div id={Data[i].id} onClick={this.handleClick}>
+                    <SpotlightCard
+                        title={Data[i].title}
+                        subtitle={Data[i].subtitle}
+                        image={Data[i].image}
+                        description={Data[i].short}>
+                    </SpotlightCard>
+                </div>
+            )
+        }
+        return posts_array;
+    }
+
+    render() {
+        return (    
+            <Aux> 
+                {
+                    this.state.gridView ? 
+                    <Aux>
+                        <h1>Search the Collections</h1>
+                        <div className={classNames(styles.grid)}>
+                            { this.pullPosts() }
+                        </div>
+                    </Aux>
+                    :
+                    <div>
+                        <p className={styles.back} onClick={this.handleBack2Grid}>&lsaquo; Back to Collections</p>
+                        <h3 className={styles.title}>{this.state.title}</h3>
+                        <img src={this.state.image} alt="Spotlight" className={classNames("imageKing", styles.imageFull)} ></img>
+                        <p dangerouslySetInnerHTML={{__html: this.state.description}} className={styles.description}></p>
+                    </div>
+                }
+            </Aux>
+        )
+    }
 }
 
 export default search;
