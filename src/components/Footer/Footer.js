efcaf95d123d139fe08db2482cf65ecac84e4662
Julia <julia.ma@yale.edu>
2018-12-11 21:09:58 -0500

    footer socials

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -52,12 +52,12 @@ const footer = (props) => {
                     </div>
                     {/* social & copyright */}
                     <div className={styles.social}>
-                        <div className={styles.icons}>
+                        <p className={styles.icons}>
                             <a href="https://www.facebook.com/YalePeabodyMuseum/" style={{textDecoration: "none"}}><FontAwesomeIcon icon={["fab", "facebook"]} size="2x"></FontAwesomeIcon></a>
                             <a href ="https://www.instagram.com/enas410peabody/" style={{textDecoration: "none"}}><FontAwesomeIcon icon={["fab", "instagram"]} size="2x"></FontAwesomeIcon></a>
                             <a href="https://twitter.com/yalepeabody?lang=en" style={{textDecoration: "none"}}><FontAwesomeIcon icon={["fab", "twitter"]} size="2x" ></FontAwesomeIcon></a>
                             <a href ="https://www.youtube.com/channel/UC5c5hS-NG0ya1cyvn85koMA" style={{textDecoration: "none"}}><FontAwesomeIcon icon={["fab", "youtube"]} size="2x"></FontAwesomeIcon></a>                          
-                        </div>
+                        </p>
                         <br/><br/>
                         <span>Copyright &copy; 2018<br/>Yale Peabody Museum <br/> of Natural History</span>
                     </div>
